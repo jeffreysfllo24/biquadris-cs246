8c27e09af6df26de4b42bd855738dbe3f5b19469
Jeffrey Zhang <jeffreysfllo24@gmail.com>
2018-11-20 16:23:05 -0500

    - Created .cc and .h files for board

---
+++ board.cc
@@ -0,0 +1,8 @@
+//
+//  board.cpp
+//  
+//
+//  Created by Jeffrey Zhang on 2018-11-20.
+//
+
+#include "board.h"
---
+++ board.h
@@ -0,0 +1,13 @@
+//
+//  board.hpp
+//  
+//
+//  Created by Jeffrey Zhang on 2018-11-20.
+//
+
+#ifndef board_hpp
+#define board_hpp
+
+#include <stdio.h>
+
+#endif /* board_hpp */
