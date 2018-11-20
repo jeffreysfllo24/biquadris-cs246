38eddd34a2090be5270cb39cf4f67716b22c7141
Jayson Lou <jayson.lou@gmail.com>
2018-11-20 17:28:17 -0500

    Added basic implementation of Cell class

--- cell.cc
+++ cell.cc
@@ -1,1 +1,20 @@
 #include "cell.h"
+
+Cell::Cell(Coordinate &c) : coord{c}, hasLetter{false} {}
+
+bool Cell::isFilled() {
+    return hasLetter;
+}
+
+char Cell::getLetter() {
+    if (hasLetter) {
+        return letter;
+    }
+    return;
+}
+
+void Cell::setLetter(const char &c) {
+    letter = c;
+}
+
+Cell::~Cell() {}
