0ed51a5b7b5774eaadea3dcfabf91da8a9db1a67
Jeffrey Zhang <jeffreysfllo24@gmail.com>
2018-11-20 20:06:27 -0500

    Added functionality to board to clear, initialize, check if row is full and delete.

--- board.cc
+++ board.cc
@@ -1,2 +1,72 @@
 #include "board.h"
-//Check Comments
+#include <string>
+using namespace std;
+
+const int width = 11;
+const int height = 18;
+const int startBoard = 3;
+Board::Board() : block{nullptr}, theGrid{} {
+}
+
+void Board::init(){
+    for(int i = 0; i < height; ++i){
+        for(int j = 0; j < width; ++j){
+            Cell * newCell = new Cell(i,j);
+            theGrid[i][j] = newCell;
+        }
+    }
+}
+
+void Board::clearBoard(){
+    for(int i = 0; i < height; ++i){
+        for(int j = 0; j < width; ++j){
+            delete theGrid[i][j];   //Delete the cell pointers in the grid
+        }
+    }
+    delete theGrid; //Delete the grid itself
+    delete block;   //Delete the block pointer in the grid
+}
+
+bool Board::isRowFull(Cell ** row){
+    for(int i = 0; i < width; ++i){
+        if (!row[i]->isFilled()){
+            return false;
+        }
+    }
+    return true;
+}
+
+void Board::clearRow(Cell ** row){
+    for(int i = 0; i < width; ++i){
+        delete row[i];
+    }
+}
+
+bool Board::isAlive(){
+    for(int i = 0; i < width; ++i){
+        if(theGrid[i][2]->isFilled()){
+            return false;   //Checks if any of the blocks in the 3 reserve rows are filled
+        }
+    }
+    return true;
+}
+
+void Board::setBlock(Block *block){
+    
+}
+
+Board::~Board(){
+    this->clearBoard();
+}
+
+string getLine(Cell ** row){
+    string res = "";
+    for(int i = 0; i < width; ++i){
+        if(row[i]->isFilled()){
+            res += row[i]->getLetter();
+        }else{
+            res += " ";
+        }
+    }
+    return res;
+}
--- board.h
+++ board.h
@@ -4,18 +4,21 @@
 #include <iostream>
 #include "block.h"
 #include "cell.h"
+#include <string>
 
-class Board {
-    Block ** blocks;
-    Cell ** theGrid;
+class Board{
+    Block *block;
+    Cell *** theGrid;
     public:
         Board();
         void clearBoard();
-        bool isRowFull();
-        void clearRows();
+        bool isRowFull(Cell **);
+        void clearRow(Cell **);
         bool isAlive();
-        void setBlock();
+        void setBlock(Block *);    //Game calls board->setBlock(block)
+        std::string getLine(Cell **);
         ~Board();
+        void init();
 };
 
 #endif /* board_hpp */
